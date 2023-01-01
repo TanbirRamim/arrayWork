e28a7249313f2893c0f3428dafa99c0a9fdc87e1
Your Tech Guy <tanbirramim420@gmail.com>
2023-01-02 01:54:03 +0600

    sorted

---
+++ sorting.cpp
@@ -0,0 +1,34 @@
+// array sorting
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int a[n];
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
